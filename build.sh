4d38c500d86cfeda05322555dedc87b4dfb6dc94
Yuxu Ge <zqr513@york.ac.uk>
2026-02-02 23:09:18 +0000

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
-./scripts/build_dist.sh 0.0.1
+#!/bin/bash
+VERSION="0.0.1"
+PLATFORM=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+case "$ARCH" in
+    x86_64) ARCH="x64" ;;
+    aarch64|arm64) ARCH="arm64" ;;
+esac
+
+./scripts/build_dist.sh "$VERSION"
 sudo rm -rf /opt/l0/*
-sudo ./dist/l0-lang-v0.0.1-darwin-arm64/install.sh /opt/l0
-source ~/.zshenv
+sudo ./dist/l0-lang-v${VERSION}-${PLATFORM}-${ARCH}/install.sh /opt/l0
+#source ~/.zshenv
